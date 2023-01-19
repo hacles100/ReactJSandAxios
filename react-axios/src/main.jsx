a9a84b31cad5fd8db5e43a266c327382e02bf3c7
haclesrocha200@gmail.com <71661551+hacles100@users.noreply.github.com>
2023-01-18 23:01:50 -0100

    finish to do the routers

--- react-axios/src/main.jsx
+++ react-axios/src/main.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import {  createBrowserRouter,  RouterProvider } from "react-router-dom";
+
+import './index.css';
+
+// Paginas
+import Home from './routes/Home';
+import NewPost from './routes/NewPost';
+
+
+// configuracao do roteamento
+
+const router = createBrowserRouter([
+  {
+    element: <App/>,
+    children: [
+
+      // rota home
+      {
+        path: "/",
+        element: <Home/>
+      },
+
+      // rota new post
+      {
+        path: "new",
+        element: <NewPost/>
+      }
+    ]
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
